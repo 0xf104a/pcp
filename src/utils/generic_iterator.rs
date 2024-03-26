92ed7760067286fc5fdd27f97e7b8bf0315b52a9
Anna-Sophie Zaitsewa <toddot@protonmail.com>
2024-03-26 18:29:09 +0100

    Minor docs edits

--- src/utils/generic_iterator.rs
+++ src/utils/generic_iterator.rs
@@ -5,6 +5,11 @@ pub trait GenericIterator<T>{
     ///
     /// Produce next object or tell that sequence is stopped
     /// 
+    /// # Returns
+    /// Option<T>: 
+    ///    * Some(T) if there is next element in sequence
+    ///    * None if there is no more elements in sequence
+    /// 
     fn internal_next(&mut self) -> Option<T>;
 }
 
--- src/writer.rs
+++ src/writer.rs
@@ -6,9 +6,39 @@ use crate::factories::WRITER_FACTORY;
 
 #[async_trait]
 pub trait Writer{
-    fn new(url: &str) -> Self where Self: Sized; 
+    /// Creates writer instance for URL
+    fn new(url: &str) -> Self where Self: Sized;
+
+    ///
+    /// Checks that Writer can write by URL
+    ///
+    /// # Arguments
+    /// * url: &str: URL to check
+    ///
+    /// # Returns
+    /// bool:
+    ///   * true if URL is writable
+    ///   * false otherwise
     fn can_write(url: &str) -> bool where Self: Sized;
+    
+    ///
+    /// Checks that given URL is directory
+    /// 
+    /// # Arguments
+    /// * url: &str: URL to check
+    /// 
+    /// # Returns
+    /// bool:
+    ///   * true if URL is directory
+    ///   * false otherwise
     fn is_directory(url: &str) -> bool where Self: Sized;
+    
+    ///
+    /// Creates given path(equivalent to mkdir -p)
+    /// 
+    /// # Arguments
+    /// * url: &str: path to create
+    /// 
     fn make_directory(url: &str) where Self: Sized;
     
     ///
@@ -33,6 +63,22 @@ pub trait Writer{
     /// assert_eq!(MyWriter::join_path(base, relative), "ftp://my-server/foo/bar/folder/file);
     /// ```
     fn join_path(base: &str, path: &str) -> String where Self: Sized;
+    
+    ///
+    /// Writes chunk of given size and returns whether write was successful
+    /// 
+    /// # Arguments
+    /// 
+    /// * chunk: &DynBuffer: reference to buffer with chunk data
+    /// * size: usize: number of bytes to write from chunk
+    /// 
+    /// # Example
+    /// ```
+    /// let mut writer = MyWriter("scheme://path/to/file");
+    /// let buffer = DynBuffer::make_buffer(42);
+    /// 
+    /// writer.write_chunk(&buffer, 42);
+    /// ```
     async fn write_chunk(&mut self, chunk: &DynBuffer, size: usize) -> std::io::Result<usize>;
 }
 
